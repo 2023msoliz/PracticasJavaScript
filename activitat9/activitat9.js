060202c21268a84ad12cbf0101a7ffc28475120b
Marco Antonio Soliz Lazarte <2023_marco.soliz@iticbcn.cat>
2023-10-23 17:21:38 +0200

    actividades

---
+++ activitat9/activitat9.js
@@ -0,0 +1,29 @@
+//Obtenir un nº entre a l'atzar entre [1-100]. Es tracta de que l'usuari l'endevini. 
+//Si l'endevina se li mostrará
+//"Obtingut en n intents" Si introdueix un zero es pintará "Game over" i sortirá. 
+//Si falla se li dirá "menor" o "major". Si introdueix algun valor invalid no posa res(es segueix)
+//pero es compte com un intent més.
+
+function activitat9() {
+const numRandom = Math.floor(Math.random() * 100) + 1;
+let intents = 0;
+
+while(true){
+    const numero = prompt("Escribe un numero random de (1-100)")
+
+    intents++;
+
+    if(numero == 0){
+        alert("Gamer over");
+        break;
+    }
+    
+    if(numero == numRandom) {
+        alert("Obtingut en " + intents + " intents");
+    } else if (numero > numRandom){
+        alert("Menor")
+    } else {
+        alert("Mayor")
+    }
+}
+}
